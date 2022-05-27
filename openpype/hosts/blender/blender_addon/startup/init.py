1c14a05b3ab1e2f5f7940c913c57286797f18dea
Félix David <felixg.david@gmail.com>
2022-05-27 17:51:06 +0200

    Handler to enable OP assets library to Blender

--- openpype/hosts/blender/blender_addon/startup/init.py
+++ openpype/hosts/blender/blender_addon/startup/init.py
@@ -1,14 +1,18 @@
 import re
+from pathlib import Path
 
 import bpy
 from bpy.app.handlers import persistent
 
 from openpype.hosts.blender.api.utils import assign_loader_to_datablocks
-from openpype.pipeline import install_host
+from openpype.pipeline import install_host, legacy_io
 from openpype.hosts.blender import api
+from openpype.pipeline.anatomy import Anatomy
 from openpype.pipeline.load.plugins import (
     discover_loader_plugins,
 )
+from openpype.settings.lib import get_project_settings
+
 
 camel_case_first = re.compile(r"^[A-Z][a-z]*")
 
@@ -50,12 +54,56 @@ def instances_purge_handler(_):
             continue
 
 
+@persistent
+def setup_asset_library(*_args):
+    """Activate OpenPype's assets library in asset browser."""
+    project_name = legacy_io.Session["AVALON_PROJECT"]
+    project_settings = get_project_settings(project_name)
+    blender_settings = project_settings.get("blender", {})
+
+    if blender_settings.get("blender-assets-library-enabled"):
+        # Get OP asset library
+        library = bpy.context.preferences.filepaths.asset_libraries.get(
+            project_name
+        )
+        if not library:
+            # Prepare anatomy data
+            anatomy = Anatomy(project_name)
+            anatomy_data = {
+                "root": anatomy.roots,
+                "project": {"name": project_name},
+            }
+            formatted_anatomy = anatomy.format(anatomy_data)
+
+            # Build folder path
+            library_folder_path = Path(
+                formatted_anatomy["blender-assets-library"]["folder"]
+            )
+
+            # Add OP assets library to asset libraries filepaths
+            bpy.ops.preferences.asset_library_add(
+                directory=library_folder_path.as_posix()
+            )
+            bpy.context.preferences.filepaths.asset_libraries[
+                -1
+            ].name = project_name
+
+
 def register():
     bpy.app.handlers.save_pre.append(loader_attribution_handler)
     bpy.app.handlers.save_pre.append(instances_purge_handler)
 
     install_host(api)
 
+    # Handlers
+    bpy.app.handlers.load_post.append(setup_asset_library)
+
 
 def unregister():
-    pass
+    # Remove OP assets library from asset libraries filepaths
+    project_name = legacy_io.Session["AVALON_PROJECT"]
+    lib_index = bpy.context.preferences.filepaths.asset_libraries.find(
+        project_name
+    )
+    if lib_index >= 0:
+        bpy.ops.preferences.asset_library_remove(lib_index)
