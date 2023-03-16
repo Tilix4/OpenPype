8a27a9e1284eed441441b6ce49430df04391c193
Félix David <felixg.david@gmail.com>
2023-03-16 13:20:07 +0100

    fix publish each review instance only

--- openpype/modules/kitsu/plugins/publish/integrate_kitsu_review.py
+++ openpype/modules/kitsu/plugins/publish/integrate_kitsu_review.py
@@ -8,14 +8,12 @@ class IntegrateKitsuReview(pyblish.api.InstancePlugin):
 
     order = pyblish.api.IntegratorOrder + 0.01
     label = "Kitsu Review"
-    # families = ["kitsu"]
+    families = ["review"]
     optional = True
 
     def process(self, instance):
-
-        context = instance.context
-        task = context.data["kitsu_task"]
-        comment = context.data.get("kitsu_comment")
+        task = instance.data["kitsu_task"]["id"]
+        comment = instance.data.get("kitsu_comment")
 
         # Check comment has been created
         if not comment:
@@ -34,6 +32,6 @@ class IntegrateKitsuReview(pyblish.api.InstancePlugin):
             self.log.debug("Found review at: {}".format(review_path))
 
             gazu.task.add_preview(
-                task, comment, review_path, normalize_movie=True
+                task, comment["id"], review_path, normalize_movie=True
             )
             self.log.info("Review upload on comment")
